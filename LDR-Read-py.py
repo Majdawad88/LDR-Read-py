cc47859e1a4806843b476326bcf0d62b5c2e81fe
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2026-01-21 15:38:54 -0500

    Create LDR-Read-py.py

---
+++ LDR-Read-py.py
@@ -0,0 +1,15 @@
+#git clone 
+
+import serial
+import csv
+import time
+
+ser = serial.Serial('COM16', 9600)
+with open('light_data.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Time", "LightLevel"])
+    for i in range(200): # Collect 200 data points
+        line = ser.readline().decode('utf-8').strip()
+        if line:
+            writer.writerow([i, int(line)])
+            print(f"Logged: {i}, {line}", flush=True)
